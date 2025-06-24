ea1519de1001aafc0af6df45bff7fddcd59707c7
Jamie M <jamiem99869@outlook.com>
2025-06-24 03:13:24 +0100

    feat: add AuditLog model & migration

---
+++ prisma/migrations/20250624021250_add_audit_log/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "AuditLog" (
+    "id" TEXT NOT NULL,
+    "action" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT,
+
+    CONSTRAINT "AuditLog_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "AuditLog" ADD CONSTRAINT "AuditLog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
